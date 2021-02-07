e0224b2d36129d7d5ed4841e1d9768a7433ae242
Serhii Staryk <serhii.staryk@gmail.com>
2021-02-07 23:03:25 +0200

    updated dragCard

--- src/components/DragCard/index.js
+++ src/components/DragCard/index.js
@@ -3,31 +3,62 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import './style.css';
 
+
 class DrugCard extends Component {
+    state = {
+        box: true
+    }
     render() {
         return (
             <Card className="drag-card">
                 <CardContent>
-                    <Typography color="textSecondary" gutterBottom>
-                        Word of the Day
-          </Typography>
-                    <Typography variant="h5" component="h2">
-                        be nev o lent
-          </Typography>
-                    <Typography color="textSecondary">
-                        adjective
-          </Typography>
-                    <Typography variant="body2" component="p">
-                        well meaning and kindly.
-            <br />
-                        {'"a benevolent smile"'}
+                    <Typography gutterBottom variant="h5" component="h2">
+                        Аквамаріс
                     </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                        2020-01-01
+                     </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Count: 1 флакон
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Dose: 15мг
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Type: спрей
+                    </Typography>
+
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        appointment: промивання носа
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Whom: Максим
+                    </Typography>
+                    <FormControlLabel
+                        className='drag-card-checbox'
+                        control={
+                            <Checkbox
+                                checked={this.state.box}
+                                checkedIcon={<CheckBoxIcon fontSize="small" />}
+                            />
+                        }
+                        disabled
+                        label="Box:"
+                        labelPlacement="start"
+                    />
                 </CardContent>
-                <CardActions>
-                    <Button size="small">Learn More</Button>
+                <CardActions className="align-button__right">
+                    <IconButton size="small" color="secondary">
+                        <ArrowUpwardIcon fontSize="inherit" />
+                    </IconButton>
                 </CardActions>
             </Card>
         )
